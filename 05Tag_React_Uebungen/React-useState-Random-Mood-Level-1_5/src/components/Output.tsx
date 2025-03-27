d71a5feb1598682e3107e43fb2d961ccae9151b7
MonaEis <mona.eismann@gmx.de>
2025-03-27 18:08:45 +0100

    Tag 5 add change colors

--- 05Tag_React_Uebungen/React-useState-Random-Mood-Level-1_5/src/components/Output.tsx
+++ 05Tag_React_Uebungen/React-useState-Random-Mood-Level-1_5/src/components/Output.tsx
@@ -1,30 +1,58 @@
 import { useState } from "react";
 import { HumanDinosaur } from "react-kawaii";
 
-const moods = ["blissful", "sad", "shocked", "happy", "lovestruck", "excited", "ko"] as const;
 
+const moods = [
+    "blissful",
+    "sad",
+    "shocked",
+    "happy",
+    "lovestruck",
+    "excited",
+    "ko",
+] as const;
 
-
+const colors = [
+    "#A6E191",
+    "#02a487",
+    "#8400f8",
+    "#f89100",
+    "#00a1f8",
+    "#f858d3",
+] as const;
 
 const Output = () => {
-        
-    const [randomNumber, setRandomNumber] = useState(0)
-    
+    const [randomNumber, setRandomNumber] = useState(0);
+
     const handleClick = () => {
-        const number = Math.floor(Math.random()*7)
-        setRandomNumber(number)
-    console.log("Randomnumber ist: ", number)}
+        const number = Math.floor(Math.random() * 7);
+        setRandomNumber(number);
+        console.log("Randomnumber ist: ", number);
+    };
+
+   
+    
+    const [randomColor, setRandomcolor] = useState("#A6E191");
+
+    const handleColorClick = () => {
+        const number = Math.floor(Math.random() * colors.length);
+        setRandomcolor(colors[number]);
+    };
     
 
     return (
-    <div className='output'>
-        <HumanDinosaur size={500} mood={moods[randomNumber]} color="#A6E191" />
+        <div className="output">
+            <HumanDinosaur
+                size={500}
+                mood={moods[randomNumber]}
+                color={randomColor}
+            />
 
-        <button onClick={handleClick}>Random Mood</button>
-        
-        
-    </div>
-  )
-}
+            <button onClick={handleClick}>Random Mood</button>
 
-export default Output
+            <button onClick={handleColorClick}>Random Color</button>
+        </div>
+    );
+};
+
+export default Output;
