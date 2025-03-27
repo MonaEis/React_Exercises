a23035ca93bb909eb2973dd194e91a320cc0883b
MonaEis <mona.eismann@gmx.de>
2025-03-27 16:37:01 +0100

    Tag 5 Übung finish

--- 05Tag_React_Uebungen/React-useState-Random-Mood-Level-1_5/src/components/Output.tsx
+++ 05Tag_React_Uebungen/React-useState-Random-Mood-Level-1_5/src/components/Output.tsx
@@ -1,39 +1,26 @@
-import React from 'react'
-
+import { useState } from "react";
 import { HumanDinosaur } from "react-kawaii";
-//   <HumanDinosaur size={240} mood="blissful" color="#A6E191" />
 
-{/* <HumanDinosaur size={240} mood="blissful" color="#A6E191" />
-        <HumanDinosaur size={240} mood="sad" color="#A6E191" />
-        <HumanDinosaur size={240} mood="shocked" color="#A6E191" />
-        <HumanDinosaur size={240} mood="happy" color="#A6E191" />
-        <HumanDinosaur size={240} mood="lovestruck" color="#A6E191" />
-        <HumanDinosaur size={240} mood="excited" color="#A6E191" />
-        <HumanDinosaur size={240} mood="ko" color="#A6E191" /> */}
-
-type Mood =  string
-
-
-const moods: Mood[] = ["blissful", "sad", "shocked", "happy", "lovestruck", "excited", "ko"];
+const moods = ["blissful", "sad", "shocked", "happy", "lovestruck", "excited", "ko"] as const;
 
 
 
 
 const Output = () => {
-    console.log("Test")
-    
-    // const [inputValue, setInputValue] = useState("blissfull")
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
         
-    }
+    const [randomNumber, setRandomNumber] = useState(0)
+    
+    const handleClick = () => {
+        const number = Math.floor(Math.random()*7)
+        setRandomNumber(number)
+    console.log("Randomnumber ist: ", number)}
+    
 
     return (
-    <div>
-        <HumanDinosaur size={300} mood="blissfull" color="#A6E191" />
+    <div className='output'>
+        <HumanDinosaur size={500} mood={moods[randomNumber]} color="#A6E191" />
 
-        <button onClick={handleSubmit}>Random Mood</button>
+        <button onClick={handleClick}>Random Mood</button>
         
         
     </div>
