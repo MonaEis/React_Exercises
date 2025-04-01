5e57c8d908932d66b5d276aa508bf97a582313de
MonaEis <mona.eismann@gmx.de>
2025-04-01 15:31:22 +0200

    add edit

--- 07_Tag_Projekt-React-FakeStore-API-Level-3_7/src/App.tsx
+++ 07_Tag_Projekt-React-FakeStore-API-Level-3_7/src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import ProductItem from "./components/ProductItem";
 
+import CircularProgress from "@mui/material/CircularProgress";
+
 export type Shop = {
     id: number;
     title: string;
@@ -10,26 +12,39 @@ export type Shop = {
 
 function App() {
     const [products, setProducts] = useState<Shop[]>([]);
+    const [loading, setLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
     useEffect(() => {
+      setLoading(true)
         fetch("https://fakestoreapi.com/products")
             .then((response) => response.json())
             .then((data) => {
                 setProducts(data);
             })
-            .catch((error) => console.log(error));
+            .catch((error) => {
+              console.log(error)
+              setErrorMessage("Es ist ein Fehler aufgetreten!")
+            }).finally(()=>{
+              setLoading(false)
+            });
     }, []);
 
     return (
         <main>
             <h1>SuperDuper-FakeShop</h1>
+                {loading ?? <CircularProgress color="secondary" />}
             <div className="output">
+                {!loading && (
                 <ul>
                     {products.map((product) => (
                         <ProductItem key={product.id} product={product} />
                     ))}
                 </ul>
+
+                )}
             </div>
+            <p className="error-message">{errorMessage}</p>
         </main>
     );
 }
