eca97fd4518db05a7fc25303c2de81a4091acd16
MonaEis <mona.eismann@gmx.de>
2025-04-03 18:13:42 +0200

    Tag 9 edit Aufgabe 2_2

--- 09_Tag_React_Routing _II/React_Dynamisches-routing/src/pages/Details.tsx
+++ 09_Tag_React_Routing _II/React_Dynamisches-routing/src/pages/Details.tsx
@@ -2,28 +2,29 @@ import { useParams } from "react-router-dom";
 import { blogData } from "../types";
 
 const Details = () => {
+    const { id } = useParams();
+    console.log(id);
 
-  const { id } = useParams();
-  console.log(id)
+    const articleToShow = blogData.filter((item) => item.id.toString() === id);
+    console.log(articleToShow);
 
-  const articleToShow = blogData.filter((item)=> item.id.toString() === id)
-  console.log(articleToShow)
+    return (
+        <section className="details">
+            {articleToShow.map((blog) => (
+                <article>
+                    <img src={blog.img_url} alt={blog.title} />
+                    <div className="infos">
+                        <div className="infos-upper">
+                            <h2>{blog.title}</h2>
+                            <h3>{blog.published_date}</h3>
+                        </div>
+                        <p>{blog.description}</p>
+                        <p>{blog.author}</p>
+                    </div>
+                </article>
+            ))}
+        </section>
+    );
+};
 
-  return (
-    <section className="details">
-      {articleToShow.map((blog)=>(
-        <article>
-        <img src={blog.img_url} alt={blog.title} />
-        <div>
-          <h2>{blog.title}</h2>
-          <p>{blog.description}</p>
-          <p>{blog.author}</p>
-        </div>
-        </article>
-      ))}
-      
-    </section>
-  )
-}
-
-export default Details
+export default Details;
